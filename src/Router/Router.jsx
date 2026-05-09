731173ffe9dd5b87c27d7559d9bd4eb7edc264e8
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-05-09 12:24:20 +0600

    _redirects file added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home/Home";
 import Root from "../Layout/Root";
 import Blogs from "../Pages/Blogs/Blogs";
-// import Calendar from "../Pages/Month/MonthlyHabits/Calender";
 import MonthPage from "../Pages/Month/MonthlyHabits/MonthPage";
 
 
